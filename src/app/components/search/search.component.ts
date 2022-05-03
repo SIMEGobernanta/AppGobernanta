b28dd48378174a6550d4f0fc831f60c4565d9100
Diego <diegofergo02@gmail.com>
2022-05-03 16:44:28 +0200

    Added separated functions for upper filters

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -9,24 +9,30 @@ import { RoomInfo } from 'src/app/room-info';
 export class SearchComponent implements OnInit {
   @Input() rooms!: RoomInfo[];
   isLoading = true;
+  aux!: RoomInfo[];
 
   constructor() { }
 
   ngOnInit(): void {
     this.isLoading = false;
+    this.aux = [...this.rooms];
   }
 
   search(value: string) {
     /*
     * La función de Carlos para los filtros de ordenación
     * SI FUNCIONA
-    this.   rooms = this.rooms.sort((a,b):number => {
+    */
+    /*
+    this.rooms = this.rooms.sort((a,b):number => {
       if(a.adults > b.adults) {return 1}
       return -1;
-    })*/
-
+    })
+    */
     //Este no actualiza el array visualmente y no se por que
-    this.rooms = this.rooms.filter(room => (room.name.includes(value)));
+    this.rooms = this.aux.filter(function(room) {
+      return room.name.includes(value);
+    });
     //El console log es correcto
     console.log(this.rooms);
   }
