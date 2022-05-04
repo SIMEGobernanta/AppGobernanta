985b816c739f1ceea2ce129d0eea3bc11f1f02e6
Diego <diegofergo02@gmail.com>
2022-05-04 12:20:59 +0200

    Added reset upper filters button (only visually)

--- src/app/components/selectors/selectores.component.ts
+++ src/app/components/selectors/selectores.component.ts
@@ -5,15 +5,8 @@ import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/mat
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const MY_DATE_FORMATS = {
-  parse: {
-    dateInput: 'DD/MM/YYYY',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY'
-  },
+  parse: {dateInput: 'DD/MM/YYYY'},
+  display: {dateInput: 'DD/MM/YYYY', monthYearLabel: 'MMMM YYYY', dateA11yLabel: 'LL', monthYearA11yLabel: 'MMMM YYYY'},
 };
 
 interface IFilters {
@@ -42,17 +35,13 @@ export class SelectoresComponent implements OnInit {
     {prop: 'houseKeeping', usedFilter:false,filterAction:[]},
     {prop: 'date', usedFilter:false, filterAction:[]},
   ];
+  usedAnyFilter: boolean = false;
 
-
-  constructor() {
-
-  }
+  constructor() { }
 
   ngOnInit(): void {
     this.minDate = moment().toDate();
   }
-
-
 
   filterBlocked(checked:boolean) {
     this.filters[0].usedFilter = !this.filters[0].usedFilter;
@@ -63,16 +52,15 @@ export class SelectoresComponent implements OnInit {
     this.filters[1].usedFilter = true;
     this.filters[1].filterAction = new Array(selected);
 
-    if (manual) {
-      this.applyFilters();
-    }
+    if (manual) this.applyFilters();
   }
-
 
   filterByDate(start:string, end:string) {
     this.filters[2].usedFilter = true;
+    //Formateo de las fechas
     const startDate = new Date(start.split('/').reverse().join('/'));
-    const endDate = new Date(end.split('/').reverse().join('/')); endDate.setHours(23); endDate.setMinutes(59); endDate.setSeconds(59);
+    const endDate = new Date(end.split('/').reverse().join('/'));
+    endDate.setHours(23); endDate.setMinutes(59); endDate.setSeconds(59);
     this.filters[2].filterAction = [startDate.toString(),endDate.toString()];
 
     this.applyFilters();
@@ -82,10 +70,11 @@ export class SelectoresComponent implements OnInit {
     //Esta cosa funciona, pero convendría cambiarlo jsjs
     const usedFilters = this.filters.filter(filter => filter.usedFilter);
     if (usedFilters.length === 0) {
-      this.resetFilters();
+      this.resetArray();
     } else {
+      this.usedAnyFilter = true;
       for (let i = 0; i < usedFilters.length; i++) {
-        //El primer filtro hay que igualarlo al array completo (rooms);
+        //El primer filtro hay que igualarlo al array completo (this.rooms);
         if (i === 0) {
           switch (usedFilters[i].prop) {
             case 'blocked':
@@ -94,12 +83,11 @@ export class SelectoresComponent implements OnInit {
             case 'houseKeeping':
               this.roomInfoAux = this.rooms.filter(room => room[usedFilters[i].prop as keyof RoomInfo] === usedFilters[i].filterAction[0]);
             break;
-
             case 'date':
               this.roomInfoAux = this.rooms.filter(room => {return room.startDate >= new Date(usedFilters[i].filterAction[0]) && room.endDate <= new Date(usedFilters[i].filterAction[1])});
             break;
           }
-          //Los que no sean el primero tienen que igualarse al array filtrado
+        //Los que no sean el primero tienen que igualarse al array filtrado por el primero (this.roomInfoAux)
         } else {
           switch (usedFilters[i].prop) {
             case 'blocked':
@@ -108,7 +96,6 @@ export class SelectoresComponent implements OnInit {
             case 'houseKeeping':
               this.roomInfoAux = this.roomInfoAux.filter(room => room[usedFilters[i].prop as keyof RoomInfo] === usedFilters[i].filterAction[0]);
             break;
-
             case 'date':
               this.roomInfoAux = this.roomInfoAux.filter(room => {return room.startDate >= new Date(usedFilters[i].filterAction[0]) && room.endDate <= new Date(usedFilters[i].filterAction[1])});
             break;
@@ -116,12 +103,21 @@ export class SelectoresComponent implements OnInit {
         }
       }
     }
+  }
 
-    console.log(this.roomInfoAux);
+  resetArray() {
+    this.roomInfoAux = this.rooms;
   }
 
   resetFilters() {
-    //I still don't know how to do
-    this.roomInfoAux = this.rooms;
+    const usedFilters = this.filters.filter(filter => filter.usedFilter);
+    if (usedFilters.length > 0) {
+      for (let i = 0; i < usedFilters.length; i++) {
+        usedFilters[i].usedFilter = false;
+      }
+    }
+    //Falta resetear el formulario
+    this.resetArray();
+    this.usedAnyFilter = false;
   }
 }
