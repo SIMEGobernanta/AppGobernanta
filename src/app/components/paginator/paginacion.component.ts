bc206169932f9182236715192379ac3a600944c6
Diego <diegofergo02@gmail.com>
2022-05-04 08:35:16 +0200

    Added pagination function

--- src/app/components/paginator/paginacion.component.ts
+++ src/app/components/paginator/paginacion.component.ts
@@ -8,10 +8,26 @@ import { RoomInfo } from 'src/app/room-info';
 })
 export class PaginacionComponent implements OnInit {
   @Input() rooms!: RoomInfo[];
+  @Input() roomAux!: RoomInfo[];
+  aux:RoomInfo[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changePage(event:any) {
+    this.aux = [...this.rooms];
+    const index = event.pageIndex;
+    const cards = event.pageSize;
+
+    if (cards <= this.rooms.length){
+      this.roomAux = this.aux.splice(index*(cards), cards);
+    } else {
+      this.roomAux = this.aux.splice(index, this.rooms.length);
+    }
+
+    console.log(this.roomAux);
   }
 
 }
